733492f2092c92db6a76902c04d22525a6051403
Rafe Bennett <rsbennett22@gmail.com>
2022-07-18 05:33:42 +0100

    Added qr generation and viewing for wetsuits

--- App/views.py
+++ App/views.py
@@ -3,13 +3,24 @@ from django.http import HttpResponse
 import qrcode
 
 # Create your views here.
-def wetsuit(request, brand, size):
-    return render(request, 'App/wetsuit.html', {'stockType' : 'wetsuit', 'size' : size, 'brand' : brand})
+def index(request):
+    return HttpResponse("Index")
 
-def createWetsuitQR(request, brand, size):
+def wetsuit(request, brand, size, gender, number):
+    fileName = brand+size+gender+number+'.png'
+    return render(request, 'App/wetsuit.html', {'stockType' : 'Wetsuit', 'brand' : brand, 'size' : size, 'gender' : gender, 'number' : number, 'fileName' : fileName})
+
+def qrForms(request):
+    return render(request, 'App/qrForms.html')
+
+def wetsuitForm(request):
+    return render(request, 'App/generateWetsuitForm.html')
+
+def generateWetsuitQR(request, brand, size, gender, number):
     #Generate qrcode from data
-    qrData = 'http://192.168.0.58:8000/wetsuit/'+brand+'&'+size
+    qrData = 'http://192.168.0.58:8000/wetsuit/'+brand+'&'+size+'&'+gender+'&'+str(number)
     qr = qrcode.make(qrData)
-    fileName = brand+size+'.png'
+    fileName = brand+size+gender+str(number)+'.png'
+    qrImg = 'qrcodes\\'+brand+size+gender+str(number)+'.png'
     qr.save('static\\qrcodes\\'+fileName)
-    return render(request, 'App/generatedQR.html', {'stockType' : 'wetsuit', 'size' : size, 'brand' : brand})
+    return render(request, 'App/wetsuit.html', {'stockType' : 'wetsuit', 'size' : size, 'brand' : brand ,'number' : number, 'gender' : gender, 'qrImg' : qrImg})
