a03c0c73190b40890448f51abac9a76551d8efec
Rafe Bennett <rsbennett22@gmail.com>
2022-07-18 07:33:25 +0100

    Fixed qr img not loading on wetsuit page

--- App/views.py
+++ App/views.py
@@ -7,8 +7,9 @@ def index(request):
     return render(request, 'App/Index.html')
 
 def wetsuit(request, brand, size, gender, number):
-    fileName = brand+size+gender+number+'.png'
-    return render(request, 'App/wetsuit.html', {'stockType' : 'Wetsuit', 'brand' : brand, 'size' : size, 'gender' : gender, 'number' : number, 'fileName' : fileName})
+    fileName = brand+str(size)+gender+str(number)+'.png'
+    qrImg = 'qrcodes\\'+brand+size+gender+str(number)+'.png'
+    return render(request, 'App/wetsuit.html', {'stockType' : 'Wetsuit', 'brand' : brand, 'size' : size, 'gender' : gender, 'number' : number, 'fileName' : fileName, 'qrImg' : qrImg})
 
 def qrForms(request):
     return render(request, 'App/qrForms.html')
@@ -20,7 +21,7 @@ def generateWetsuitQR(request, brand, size, gender, number):
     #Generate qrcode from data
     qrData = 'http://192.168.0.58:8000/wetsuit/'+brand+'&'+size+'&'+gender+'&'+str(number)
     qr = qrcode.make(qrData)
-    fileName = brand+size+gender+str(number)+'.png'
+    fileName = brand+str(size)+gender+str(number)+'.png'
     qrImg = 'qrcodes\\'+brand+size+gender+str(number)+'.png'
     qr.save('static\\qrcodes\\'+fileName)
     return render(request, 'App/wetsuit.html', {'stockType' : 'wetsuit', 'size' : size, 'brand' : brand ,'number' : number, 'gender' : gender, 'qrImg' : qrImg})
