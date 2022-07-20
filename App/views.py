fd66ab9fce13079acacc71cbef8cddb07615b805
Rafe Bennett <rsbennett22@gmail.com>
2022-07-20 01:01:25 +0100

    Added home button to all pages

--- App/views.py
+++ App/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 import qrcode
+from os.path import exists
 
 # Create your views here.
 def index(request):
@@ -9,8 +10,8 @@ def index(request):
 
 def wetsuit(request, brand, size, gender, number):
     fileName = brand+str(size)+gender+str(number)+'.png'
-    qrImg = 'qrcodes\\'+brand+size+gender+str(number)+'.png'
-    return render(request, 'App/wetsuit.html', {'stockType' : 'Wetsuit', 'brand' : brand, 'size' : size, 'gender' : gender, 'number' : number, 'fileName' : fileName, 'qrImg' : qrImg})
+    qrPath = 'qrcodes\\'+brand+str(size)+gender+str(number)+'.png'
+    return render(request, 'App/wetsuit.html', {'stockType' : 'Wetsuit', 'brand' : brand, 'size' : size, 'gender' : gender, 'number' : number, 'fileName' : fileName, 'qrPath' : qrPath})
 
 def qrForms(request):
     return render(request, 'App/qrForms.html')
@@ -19,10 +20,26 @@ def wetsuitForm(request):
     return render(request, 'App/generateWetsuitForm.html')
 
 def generateWetsuitQR(request, brand, size, gender, number):
-    #Generate qrcode from data
-    qrData = 'http://192.168.0.58:8000/wetsuit/'+brand+'&'+str(size)+'&'+gender+'&'+str(number)
-    qr = qrcode.make(qrData)
     fileName = brand+str(size)+gender+str(number)+'.png'
-    qrImg = 'qrcodes\\'+brand+size+gender+str(number)+'.png'
-    qr.save('static\\qrcodes\\'+fileName)
-    return render(request, 'App/redirectToWetsuitPage.html', {'stockType' : 'wetsuit', 'size' : size, 'brand' : brand ,'number' : number, 'gender' : gender, 'qrImg' : qrImg, 'fileName' : fileName})
+    qrPath = 'qrcodes\\'+brand+size+gender+str(number)+'.png'
+    print("Checking if QR code has already been generated...")
+    if(checkForQR(fileName)):
+        print("Matching QR code found. Redirecting to wetsuit info page...")
+        return render(request, 'App/redirectToWetsuitPage.html', {'stockType' : 'wetsuit', 'size' : size, 'brand' : brand ,'number' : number, 'gender' : gender, 'qrPath' : qrPath, 'fileName' : fileName})
+    else:
+        print("QR code not found")
+        print("Generating QR code...")
+        #Generate qrcode from data
+        qrData = 'http://192.168.0.58:8000/wetsuit/'+brand+'&'+str(size)+'&'+gender+'&'+str(number)
+        qr = qrcode.make(qrData)
+        qr.save('static\\qrcodes\\'+fileName)
+        print("QR code generated. Redirecting to wetsuit info page...")
+        return render(request, 'App/redirectToWetsuitPage.html', {'stockType' : 'wetsuit', 'size' : size, 'brand' : brand ,'number' : number, 'gender' : gender, 'qrPath' : qrPath, 'fileName' : fileName})
+
+def checkForQR(fileName):
+    path = 'static\\qrcodes\\'+fileName
+    print(path)
+    if(exists(path)):
+        return True
+    else:
+        return False
