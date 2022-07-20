421019e4772715094eb1a857c770789f155483eb
Rafe Bennett <rsbennett22@gmail.com>
2022-07-20 21:42:36 +0100

    Added print statements to show progress of program in console

--- App/urls.py
+++ App/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('wetsuit/<str:brand>&<str:gender>&<str:size>&<int:number>', views.wetsuit, name='wetsuit'),
-    path('qrFormsPage/', views.qrForms, name='qrFormsPage'),
+    path('stockFormsPage/', views.stockForms, name='stockFormsPage'),
     path('wetsuitFormPage/', views.wetsuitForm, name='wetsuitFormPage'),
     path('addNewWetsuit/', views.addNewWetsuit, name='addNewWetsuit'),
     path('deleteItem/', views.deleteItem, name='deleteItem'),
--- App/views.py
+++ App/views.py
@@ -1,3 +1,5 @@
+from base64 import b32hexdecode
+from email.quoprimime import body_check
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 import qrcode
@@ -5,67 +7,113 @@ from os.path import exists
 from os import remove
 from .models import StockItem, Wetsuit
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def index(request):
-    return render(request, 'App/Index.html')
+    return render(request, 'App/Index.html',print(bcolors.OKBLUE+'Successfully loaded home page!'+bcolors.ENDC))
 
 def wetsuit(request, brand, gender, size, number):
-    fileName = brand+gender+str(size)+str(number)+'.png'
-    print("Checking for QR code...")
-    if(checkForQR(fileName)):
-        #If QR code in image folder, load info page
-        print("QR code found. Loading info page")
-        qrPath = 'qrcodes\\'+brand+gender+str(size)+str(number)+'.png'
-        #Get pk of this wetsuit
-        thisWetsuit = Wetsuit.objects.get(brand=brand, gender=gender, size=size, number=number)
-        pk = thisWetsuit.pk
-        return render(request, 'App/wetsuit.html', {'stockType' : 'wetsuit', 'brand' : brand, 'gender' : gender, 'size' : size, 'number' : number, 'qrPath' : qrPath, 'pk' : pk})
-    else:
-        #If QR code not in image folder, load error page
-        print("QR code not found!")
-        return render(request, 'App/qrErrorPage.html')
 
-def qrForms(request):
-    return render(request, 'App/qrForms.html')
+    #REMOVED QR CHECK HERE
+
+    qrPath = 'qrcodes\\'+brand+gender+str(size)+str(number)+'.png'
+    #Get pk of this wetsuit
+    print(bcolors.OKGREEN+"Getting pk of this wetsuit object..."+bcolors.ENDC)
+    thisWetsuit = Wetsuit.objects.get(brand=brand, gender=gender, size=size, number=number)
+    pk = thisWetsuit.pk
+    if(pk!=None):
+        print(bcolors.OKBLUE+"PK succesfully obtained!"+bcolors.ENDC)
+        print(bcolors.OKGREEN+"Loading info page"+bcolors.ENDC)
+        return render(request, 'App/wetsuit.html', {'stockType' : 'wetsuit', 'brand' : brand, 'gender' : gender, 'size' : size, 'number' : number, 'qrPath' : qrPath, 'pk' : pk}, print(bcolors.OKBLUE+"Successfully loaded wetsuit info page!"+bcolors.ENDC))
+    else:
+        print(bcolors.FAIL+"Error, PK is None!")
+        return render(request, 'App/pkErrorPage.html', print(bcolors.WARNING+'Successfully loaded pk error page'+bcolors.ENDC))
+
+def stockForms(request):
+    print(bcolors.OKBLUE+"Successfully loaded stock form selection page!"+bcolors.ENDC)
+    return render(request, 'App/stockForms.html')
 
 def wetsuitForm(request):
-    return render(request, 'App/generateWetsuitForm.html')
+    return render(request, 'App/generateWetsuitForm.html', print(bcolors.OKBLUE+"Successfully loaded wetsuit form page!"+bcolors.ENDC))
 
 def addNewWetsuit(request):
     if(request.method=='POST'):
         if(request.POST.get('brand')):
             newWetsuit=Wetsuit()
             newWetsuit.stockType='wetsuit'
+
+            print(bcolors.OKGREEN+"Getting wetsuit brand..."+bcolors.ENDC)
             newWetsuit.brand=request.POST.get('brand')
+            if(newWetsuit.brand!=None):
+                print(bcolors.OKBLUE+"Successfully obtained wetsuit brand: "+bcolors.ENDC+newWetsuit.brand)
+            else:
+                return print(bcolors.FAIL+"Error, brand is None!"+bcolors.ENDC)
+            
+            print(bcolors.OKGREEN+"Getting wetsuit gender..."+bcolors.ENDC)
             newWetsuit.gender=request.POST.get('gender')
+            if(newWetsuit.gender!=None):
+                print(bcolors.OKBLUE+"Successfully obtained wetsuit gender: "+bcolors.ENDC+newWetsuit.gender)
+            else:
+                return print(bcolors.FAIL+"Error, gender is None!"+bcolors.ENDC)
+            
+            print(bcolors.OKGREEN+"Getting wetsuit size..."+bcolors.ENDC)
             newWetsuit.size=request.POST.get('size')
+            if(newWetsuit.size!=None):
+                print(bcolors.OKBLUE+"Successfully obtained wetsuit size: "+bcolors.ENDC+newWetsuit.size)
+            else:
+                return print(bcolors.FAIL+"Error, size is None!"+bcolors.ENDC)
+            
+            print(bcolors.OKGREEN+"Getting wetsuit number..."+bcolors.ENDC)
             newWetsuit.number=request.POST.get('num')
+            if(newWetsuit.number!=None):
+                print(bcolors.OKBLUE+"Successfully obtained wetsuit number: "+bcolors.ENDC+newWetsuit.number)
+            else:
+                return print(bcolors.FAIL+"Error, number is None!"+bcolors.ENDC)
+            
             fileName=newWetsuit.brand+newWetsuit.gender+newWetsuit.size+newWetsuit.number+'.png'
-            print(newWetsuit.brand)
-            print(newWetsuit.gender)
-            print(newWetsuit.size)
-            print(newWetsuit.number)
+            print(bcolors.OKGREEN+"Checking if QR code matching "+fileName+" exists..."+bcolors.ENDC)
             #Check if QR code matching info exists
             if(checkForQR(fileName)):
                 #Load wetsuit page wetsuit item's info
+                print(bcolors.OKBLUE+"QR code already exists!"+bcolors.ENDC)
+                print(bcolors.OKGREEN+"Loading wetsuit info page..."+bcolors.ENDC)
                 return wetsuit(request, newWetsuit.brand, newWetsuit.gender, newWetsuit.size, newWetsuit.number)
             #Create QR code from data
             else:
+                print(bcolors.WARNING+"QR code does not exist!"+bcolors.ENDC)
                 generateWetsuitQR(newWetsuit.brand, newWetsuit.gender, newWetsuit.size, newWetsuit.number, fileName)
                 #Add qr code to model instance
                 newWetsuit.qrCode=fileName
+                print(bcolors.OKGREEN+"Adding new wetsuit to database..."+bcolors.ENDC)
                 newWetsuit.save()
+                print(bcolors.OKBLUE+"Successfully added new wetsuit to database!"+bcolors.ENDC)
                 #Redirect to wetsuit info page
+                print(bcolors.OKGREEN+"Loading wetsuit info page..."+bcolors.ENDC)
                 return wetsuit(request, newWetsuit.brand, newWetsuit.gender, newWetsuit.size, newWetsuit.number)
         else:
             return HttpResponse('Bad request...')
 
 def generateWetsuitQR(brand, gender, size, number, fileName):
-    print("Generating a new wetsuit QR code...")
+    print(bcolors.OKGREEN+"Generating a new wetsuit QR code..."+bcolors.ENDC)
     #Generate qrcode from data
     qrData = 'http://192.168.0.58:8000/wetsuit/'+brand+'&'+gender+'&'+str(size)+'&'+str(number)
     qr = qrcode.make(qrData)
-    qr.save('static\\qrcodes\\'+fileName)
-    print("QR code generated. Redirecting to wetsuit page...")
+    print(bcolors.OKGREEN+"Saving generated QR code..."+bcolors.ENDC)
+    path = 'static\\qrcodes\\'+fileName
+    qr.save(path)
+    if(exists(path)):
+        return print(bcolors.OKBLUE+"Successfully generated and saved QR code!"+bcolors.ENDC)
+    else:
+        return print(bcolors.FAIL+"QR code failed to save!"+bcolors.ENDC)
 
 def checkForQR(fileName):
     path = 'static\\qrcodes\\'+fileName
