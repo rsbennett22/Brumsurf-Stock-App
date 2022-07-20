01b3d6d0b4b3a7099016185de6c1b1ca899cab4f
Rafe Bennett <rsbennett22@gmail.com>
2022-07-20 05:37:57 +0100

    Implemented models and rewrote QR gen

--- App/models.py
+++ App/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MinLengthValidator
-
 # Create your models here.
 
 #Stock model info:
@@ -20,39 +19,21 @@ from django.core.validators import MinValueValidator, MinLengthValidator
 #Name
 #Student ID
 
-class stockItem(models.Model):
+class StockItem(models.Model):
+    stockType = models.CharField(max_length=15)
     brand = models.CharField(max_length=30)
     size = models.CharField(max_length=10)
-    Number = models.PositiveBigIntegerField(default=0, validators=[MinValueValidator(1)])
+    number = models.PositiveBigIntegerField(default=0, validators=[MinValueValidator(1)])
     onTrip = models.BooleanField(default=False)
     signedOut = models.BooleanField(default=False)
     signedIn = models.BooleanField(default=True)
     name = models.CharField(default='brumsurf', max_length=50)
     studentId = models.CharField(default='0000000', max_length=7, validators=[MinLengthValidator(7)])
 
-class wetsuit(stockItem):
-    MALE_SIZES = [
-        ('S', 'Small'), 
-        ('M', 'Medium'), 
-        ('MT', 'Medium-Tall'), 
-        ('L', 'Large'), 
-        ('XL', 'X-Large')
-        ]
-    FEMALE_SIZES = [
-        ('4', 'Four'), 
-        ('6', 'Six'), 
-        ('8', 'Eight'), 
-        ('10', 'Ten'), 
-        ('12', 'Twelve'), 
-        ('14', 'Fourteen'), 
-        ('16', 'Sixteen')
-        ]
-    GENDERS = [
-        ('M', 'Male'), 
-        ('F', 'Female')
-        ]
-    gender = models.CharField(max_length=6, choices=GENDERS)
-    if(gender=='Male'):
-        wetsuitSize = models.CharField(max_length=10, choices=MALE_SIZES)
-    else:
-        wetsuitSize = models.CharField(max_length=10, choices=FEMALE_SIZES)
+    def __str__(self):
+        return (self.stockType+'_'+self.brand+'_'+str(self.number))
+
+class Wetsuit(StockItem):
+    size=None
+    gender = models.CharField(max_length=6)
+    wetsuitSize = models.CharField(max_length=10)
--- App/views.py
+++ App/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 import qrcode
 from os.path import exists
+from .models import StockItem, Wetsuit
 
 # Create your views here.
 def index(request):
     return render(request, 'App/Index.html')
 
-def wetsuit(request, brand, size, gender, number):
-    fileName = brand+str(size)+gender+str(number)+'.png'
+def wetsuit(request, brand, gender, size, number):
+    fileName = brand+gender+str(size)+str(number)+'.png'
     print("Checking for QR code...")
     if(checkForQR(fileName)):
         #If QR code in image folder, load info page
         print("QR code found. Loading info page")
-        qrPath = 'qrcodes\\'+brand+str(size)+gender+str(number)+'.png'
-        return render(request, 'App/wetsuit.html', {'stockType' : 'Wetsuit', 'brand' : brand, 'size' : size, 'gender' : gender, 'number' : number, 'fileName' : fileName, 'qrPath' : qrPath})
+        qrPath = 'qrcodes\\'+brand+gender+str(size)+str(number)+'.png'
+        return render(request, 'App/wetsuit.html', {'stockType' : 'Wetsuit', 'brand' : brand, 'gender' : gender, 'size' : size, 'number' : number, 'fileName' : fileName, 'qrPath' : qrPath})
     else:
         #If QR code not in image folder, load error page
         print("QR code not found!")
@@ -27,7 +28,7 @@ def qrForms(request):
 def wetsuitForm(request):
     return render(request, 'App/generateWetsuitForm.html')
 
-def generateWetsuitQR(request, brand, size, gender, number):
+###def generateWetsuitQR(request, brand, size, gender, number):
     fileName = brand+str(size)+gender+str(number)+'.png'
     qrPath = 'qrcodes\\'+brand+size+gender+str(number)+'.png'
     print("Checking if QR code has already been generated...")
@@ -43,6 +44,42 @@ def generateWetsuitQR(request, brand, size, gender, number):
         qr.save('static\\qrcodes\\'+fileName)
         print("QR code generated. Redirecting to wetsuit info page...")
         return render(request, 'App/redirectToWetsuitPage.html', {'stockType' : 'wetsuit', 'size' : size, 'brand' : brand ,'number' : number, 'gender' : gender, 'qrPath' : qrPath, 'fileName' : fileName})
+
+def addNewWetsuit(request):
+    if(request.method=='POST'):
+        if(request.POST.get('brand')):
+            newWetsuit=Wetsuit()
+            newWetsuit.stockType='Wetsuit'
+            newWetsuit.brand=request.POST.get('brand')
+            newWetsuit.gender=request.POST.get('gender')
+            newWetsuit.wetsuitSize=request.POST.get('size')
+            newWetsuit.number=request.POST.get('num')
+
+            fileName=newWetsuit.brand+newWetsuit.gender+newWetsuit.wetsuitSize+newWetsuit.number+'.png'
+            print(newWetsuit.brand)
+            print(newWetsuit.gender)
+            print(newWetsuit.wetsuitSize)
+            print(newWetsuit.number)
+            #check if QR code matching info exists
+            if(checkForQR(fileName)):
+                #load wetsuit page wetsuit item's info
+                return wetsuit(request, newWetsuit.brand, newWetsuit.gender, newWetsuit.wetsuitSize, newWetsuit.number)
+            #create QR code from data
+            else:
+                generateWetsuitQR(newWetsuit.brand, newWetsuit.gender, newWetsuit.wetsuitSize, newWetsuit.number, fileName)
+                newWetsuit.save()
+                #redirect to wetsuit info page
+                return wetsuit(request, newWetsuit.brand, newWetsuit.gender, newWetsuit.wetsuitSize, newWetsuit.number)
+        else:
+            return HttpResponse('Bad request...')
+
+def generateWetsuitQR(brand, gender, size, number, fileName):
+    print("Generating a new wetsuit QR code...")
+    #Generate qrcode from data
+    qrData = 'http://192.168.0.58:8000/wetsuit/'+brand+'&'+gender+'&'+str(size)+'&'+str(number)
+    qr = qrcode.make(qrData)
+    qr.save('static\\qrcodes\\'+fileName)
+    print("QR code generated. Redirecting to wetsuit page...")
 
 def checkForQR(fileName):
     path = 'static\\qrcodes\\'+fileName
