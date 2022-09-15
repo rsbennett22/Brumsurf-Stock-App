27875ff3012f5efdc62ab382bc09231b0fef31dd
Rafe <rafebennett22@gmail.com>
2022-09-15 22:07:09 +0100

    Shutting up dependabot

--- Brumsurf_Stock_App/settings_local.py
+++ Brumsurf_Stock_App/settings_local.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7qw^)%o6z9wjj4vk+ear+1bbzxsnlc+%%quo*sc(bhcdb$5-xt'
+SECRET_KEY = SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
